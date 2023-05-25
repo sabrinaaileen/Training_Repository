e9a42466888e53a0a26c1fdc9a2c35b0d326a558
sabrinaaileen <115700137+sabrinaaileen@users.noreply.github.com>
2023-05-25 14:35:38 +0200

    Added simple search engine with fake data

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Weather() {
-  return <h2>This is React Weather</h2>;
+  let [city, setCity] = useState("");
+  let [documentation, setDocumentation] = useState("");
+  function handleSearch(event) {
+    event.preventDefault();
+    if (city.length > 0) {
+      return setDocumentation(`It is 19°C in ${city}`);
+    } else {
+      alert("Enter a city");
+    }
+  }
+  function changeCity(event) {
+    setCity(event.target.value);
+  }
+  return (
+    <div>
+      <h2>This is React Weather</h2>
+      <form onSubmit={handleSearch} className="weatherForm">
+        <input type="text" placeholder="Enter a city" onChange={changeCity} />
+        <input type="submit" value="Search" />
+      </form>
+      <p>{documentation}</p>
+    </div>
+  );
 }
