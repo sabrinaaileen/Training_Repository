8798229766c51daf9139aac3564a960c757e90b8
sabrinaaileen <115700137+sabrinaaileen@users.noreply.github.com>
2023-05-26 20:17:48 +0200

    import axios + create function showTemperature

--- src/Weather.js
+++ src/Weather.js
@@ -1,12 +1,19 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 export default function Weather() {
   let [city, setCity] = useState("");
   let [documentation, setDocumentation] = useState("");
+  let [temperature, setTemperature] = useState("");
+  function showTemperature(response) {
+    setTemperature(response.data.main.temp);
+  }
   function handleSearch(event) {
     event.preventDefault();
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=cb286bad3607984b41ed10c8de5cf00e`;
+    axios.get(url).then(showTemperature);
     if (city.length > 0) {
-      return setDocumentation(`It is 19°C in ${city}`);
+      return setDocumentation(`It is ${Math.round(temperature)}°C in ${city}`);
     } else {
       alert("Enter a city");
     }
